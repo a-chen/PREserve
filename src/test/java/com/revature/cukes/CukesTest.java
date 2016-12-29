64063ec3caa8e8133a4418e219c4f8eb95e0d5d9
Rikki Katz <katz.rikki@gmail.com>
2016-12-29 15:20:42 -0500

    added cucumber tests

---
+++ src/test/java/com/revature/cukes/CukesTest.java
@@ -0,0 +1,10 @@
+package com.revature.cukes;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features={"src/main/resources/reserve.feature"})
+public class CukesTest {}	// hook class
