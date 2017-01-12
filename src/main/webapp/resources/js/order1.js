d6caa935628a9861f494798cd6ab6a9e8212d0d6
Andrew Chen <achen.this@gmail.com>
2017-01-11 21:43:03 -0500

    Minor update

--- src/main/webapp/resources/js/order1.js
+++ src/main/webapp/resources/js/order1.js
@@ -6,20 +6,18 @@ $(document).ready(function () {
         loadOrder(1)
     });
 
+    // Populates existing items for given order
     function loadOrder(reservationId) {
         console.log("loadOrder starting with id: " + reservationId);
-
-        var response;
 
         $.ajax({
             type: 'GET',
             url: '/order?q=' + reservationId,
             dataType: 'json',
             success: function (data) {
-                console.log("Reservations retrieved");
-                response = data;
+                console.log("Order retrieved");
 
-                populatePreorderedList(reservationId, response);
+                populatePreorderedList(reservationId, data);
 
                 /*$.each(response, function (index, order) {
                     // console.log(order.reservation);
@@ -48,26 +46,24 @@ $(document).ready(function () {
          });
          }
          });*/
-
-
-    }
+    } // /loadOrder
 
     function populatePreorderedList(reservationId, response) {
 
         var preorderedItems;
 
-
         preorderedItems = '<!-- Pre-ordered items --> ' +
             '<div class="ordered-items-container"> ' +
-            '<h3>Ordered items</h3> ' +
-            '<table class="table ordered-items"> ' +
-            '<tr> ' +
-            '<th>Price</th> ' +
-            '<th>Name</th> ' +
-            '<th>Description</th> ' +
-            '</tr> ' +
-            '<!-- set item number with item number from db --> ' +
-            '<!-- upon click anywhere on a table row, will add that item to order --> ';
+                '<h3>Ordered items</h3> ' +
+                '<table class="table ordered-items"> ' +
+                    '<tr> ' +
+                        '<th>Quantity</th>' +
+                        '<th>Price</th> ' +
+                        '<th>Name</th> ' +
+                        '<th>Description</th> ' +
+                    '</tr> ' +
+                    '<!-- set item number with item number from db --> ' +
+                    '<!-- upon click anywhere on a table row, will add that item to order --> ';
 
         $.each(response, function (index, order) {
             console.log("order.reservation.id:" + order.reservation.id + ", order.reservation: " + reservationId);
@@ -75,24 +71,34 @@ $(document).ready(function () {
                 $.each(order.orderItems, function (key1, orderItem) {
                     preorderedItems +=
                         '<tr class="item-" + orderItem.item.id> ' +
-                        '<td>' + orderItem.item.price + '</td> ' +
-                        '<td>' + orderItem.item.name + '</td> ' +
-                        '<td>' + orderItem.item.description + '</td> ' +
+                            '<td>' + orderItem.quantity + '</td>' +
+                            '<td>' + orderItem.item.price + '</td> ' +
+                            '<td>' + orderItem.item.name + '</td> ' +
+                            '<td>' + orderItem.item.description + '</td> ' +
                         '</tr> ';
                 });
             }
         });
 
-        preorderedItems += '</table> ' +
-            '<!-- auto update this every time something is added to #ordered-items --> ' +
-            '<h5>Total: <span class="menu-total">12</span></h5> ' +
+        preorderedItems +=
+                '</table> ' +
+                '<!-- auto update this every time something is added to #ordered-items --> ' +
+                '<h5>Total: <span class="menu-total">12</span></h5> ' +
             '</div>';
 
         console.log(preorderedItems);
 
         //e.g. $("#reservation-1 .table-selector")
         $("#reservation-" + reservationId + " .table-selector").after(preorderedItems);
+    } // /populatePreorderedList
+
+    //Populates menus with items
+    function loadMenu() {
+
+
 
     }
+
+
 
 });
